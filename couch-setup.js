84cfa548e0a06a66f0a51a4978549652f5a4fe9d
Jared Hirsch <ohai@6a68.net>
2013-08-17 23:47:19 -0700

    Dont barf if database exists.

--- couch-setup.js
+++ couch-setup.js
@@ -1,8 +1,13 @@
-var nano = require('nano'),
-  log = process.env['LOG_LIBRARY'] ? require(process.env['LOG_LIBRARY']) : console.log,
+var nano = require('nano')(process.env['COUCHDB_URL'] || 'http://localhost:5984'),
+  log = process.env['LOG_LIBRARY'] ? require(process.env['LOG_LIBRARY']) : console.log;
 
 // TODO only try to create if it doesn't exist
-nano.db.create('harfiles', function(err, body) {
-  if (err) { return log('EMERGENCY', 'Unable to create "harfiles" couch database: ' + err); }
-  log('INFO', 'Created "harfiles" couch database');
+nano.db.create('harfiles', function(err, body, header) {
+  if (err == 'Error: The database could not be created, the file already exists.') {
+    log('INFO', '"harfiles" couch database already exists');
+  } else if (err) {
+    log('EMERGENCY', 'Unable to create "harfiles" couch database: ' + err);
+  } else { 
+    log('INFO', 'Created "harfiles" couch database');
+  }
 });
