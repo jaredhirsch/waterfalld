27962f80928ab8405962a1009c5a30b43867082b
Jared Hirsch <ohai@6a68.net>
2013-08-17 21:41:16 -0700

    fuck I love node. so easy to get going

---
+++ README.md
@@ -0,0 +1,9 @@
+**w**hiskey **p**apa **t**ango. distributed performance monitoring using webpagetest.
+
+list of urls/browsers -> webpagetest -> HAR files & other JSON data -> document storage -> metrics yay.
+
+a mozilla persona friday project, provoked by a conversation on [the mailing list](https://groups.google.com/forum/#!msg/mozilla.dev.identity/D59f9m72eyc/_ogjpnrFyZ8J).
+
+license: apache 2.
+
+pull requests welcome.
---
+++ index.js
@@ -0,0 +1,99 @@
+// process envs you can set:
+//
+// CONFIG_FILE = path to config file matching format in config.json.sample.
+//               any path compatible with fs.readFileSync works.
+//
+// URL_FILE = path to config file containing a JSON list of urls to test.
+//
+// BROWSER_FILE = path to config file containing a JSON list of locations/browsers to use
+//
+// LOG_LIBRARY = name of the library you use for logging. any package name or
+//               library path compatible with require() works.
+//               Note that logs pass a syslog level (eg, 'EMERGENCY', 'ALERT')
+//               as a first argument, then a message as a second argument. Your
+//               logger must accept multiple args (or concat them, whatever).
+
+var fs = require('fs'),
+  log = process.env['LOG_LIBRARY'] ? require(process.env['LOG_LIBRARY']) : console.log,
+  WebPageTest = require('webpagetest'),
+  cfgFile = process.env['CONFIG_FILE'] || './config.json',
+  urlFile = process.env['URL_FILE'] || './urls.json',
+  browserFile = process.env['BROWSER_FILE'] || './browsers.json',
+  config = loadConfig(cfgFile);
+  urls = loadConfig(urlFile),
+  browsers = loadConfig(browserFile),
+  pendingTestsQ = [],
+  wpt = new WebPageTest('www.webpagetest.org');
+
+// TODO surely some library exists that thoughtfully loads a file
+function loadConfig(configFile) {
+  var configFileContents, parsedConfig;
+  try {
+    configFileContents = fs.readFileSync(configFile);
+  } catch (e) {
+    return log('EMERGENCY', 'unable to load config file: ' + e);
+  }
+
+  try {
+    parsedConfig = JSON.parse(configFileContents)
+  } catch (e) {
+    return log('EMERGENCY', 'unable to parse config file: ' + e);
+  }
+
+  return parsedConfig;
+}
+
+// add urls X browsers onto pending queue.
+function loadTests() {
+  urls.each(function(url) {
+    browsers.each(function(browser) {
+      var args = [
+        url,
+        { key: config.apikey
+          , browser: browser /* eg 'SanJose_IE9' - both location and browser */
+          , firstViewOnly: true /* TODO do we care about warmed cache? */
+          , pollResults: 15 /* poll every 15 sec for results */
+          , timeout: 300 /* give up after 5 minutes */
+        },
+        onRunTest
+      ];
+      log('DEBUG', 'loading onto pendingTestsQ: ' + args);
+      pendingTestsQ.push(args);
+    });
+  });
+
+  start();
+}
+
+// TODO better name? runTests sounds too much like wpt's runTest.
+function start() {
+  if (!pendingTestsQ.length) { return log('INFO', 'All pending tests have been sent to the wpt server.'); }
+  var next = pendingTestsQ.shift();
+  log('INFO', 'Sending a runTest call to the wpt server: ' + next);
+  wpt.runTest.call(wpt, next);
+  setTimeout(runTests, config.request_delay || 15000);
+}
+
+// wpt.runTest callback
+function onRunTest(err, data) {
+  if (err) { return log('ERROR', 'failed to run test. error: ' + err); }
+  if (data.response.statusCode != 200) { return log('ERROR', 'non-200 status code in runTest response: ' + data); }
+  var id = data.response.data.testId;
+  log('INFO', 'Notified by wpt server that job ' + id + ' has run. Now fetching HAR data.');
+  wpt.getHARData(id, onGetHARData(id));
+}
+
+// wpt.getHARData callback
+function onGetHARData(test_id) {
+  return function(err, data) {
+    if (err) { return log('ERROR', 'failed to get HAR data for test ' + test_id + '. error: ' + err); }
+    log('INFO', 'HAR data received for test job ' + test_id + '. Now writing to disk.');
+    // TODO enable other transports than just writing to local disk.
+    fs.writeFile(test_id + '.har', data, function(err) {
+      if (err) { return log('EMERGENCY', 'unable to write results file for test ' + test_id + '. error: ' + err); }
+      log('INFO', 'HAR data written to disk for test job ' + test_id + '.');
+    });
+  };
+}
+
+loadTests()
