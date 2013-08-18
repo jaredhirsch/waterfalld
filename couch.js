9470528874f85c951ee7289776feaa9942eebe4f
Jared Hirsch <ohai@6a68.net>
2013-08-18 00:15:26 -0700

    Make the couch work

--- couch.js
+++ couch.js
@@ -1,11 +1,12 @@
-var nano = require('nano'),
+var nano = require('nano')(process.env['COUCHDB_URL'] || 'http://localhost:5984'),
   log = process.env['LOG_LIBRARY'] ? require(process.env['LOG_LIBRARY']) : console.log,
-  server = nano(process.env['COUCHDB_URL'] || 'http://localhost:5984'),
-  db = server.use('harfiles');
+  db = nano.use('harfiles');
 
 module.exports = {
-  save: function(doc) { db.insert(doc, function(err,body) {
-    if (err) { log('EMERGENCY', 'unable to insert doc: ' + err); }
-    log('DEBUG', 'inserted doc into DB: ' + body);
-  });
+  save: function(doc) {
+    db.insert(doc, function(err,body) {
+      if (err) { log('EMERGENCY', 'unable to insert doc: ' + err); }
+      log('DEBUG', 'inserted doc into DB: ' + body);
+    });
+  }
 }
