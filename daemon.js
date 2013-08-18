a906c36b55eae9dd2ee82145b2ef4ef0aa80e1e3
Jared Hirsch <ohai@6a68.net>
2013-08-18 12:21:06 -0700

    Improve daemon logging.

--- daemon.js
+++ daemon.js
@@ -1,18 +1,24 @@
 var forever = require('forever-monitor'),
-  log = process.env['LOG_LIBRARY'] ? require(process.env['LOG_LIBRARY']) : console.log;
+  log = process.env['LOG_LIBRARY'] ? require(process.env['LOG_LIBRARY']) : console.log,
+  maxRestarts = 3; // TODO extract config file code so we can do "config.maxRestarts" here
 
 var child = new (forever.Monitor)('index.js', {
-  max: 3,
+  max: maxRestarts,
   silent: false,
   options: []
 });
 
+child.on('start', function() { log('INFO', 'forever-monitor starting wpt process') });
+child.on('restart', function() { log('INFO', 'forever-monitor restarting wpt process') });
+child.on('stop', function() { log('INFO', 'forever-monitor caught stop signal, shutting down wpt process.') })
+
 child.on('error', function(err) {
-  log('EMERGENCY', 'wpt process has exited with an error: ' + err);
+  log('EMERGENCY', 'forever-monitor detected wpt process threw an error: ' + err);
   // TODO send email on exit
 });
 child.on('exit', function () {
-  log('INFO', 'wpt process has exited after 3 restarts');
+  // TODO does this emit 'exit' under any other circumstances?
+  log('INFO', 'forever-monitor detected wpt process has exited the max number of times (' + maxRestarts + '), exiting instead of restarting.');
   // TODO send email on exit
 });
 
