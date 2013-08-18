6af74af22a7a4138b1edeb381063e3595203bdbc
Jared Hirsch <ohai@6a68.net>
2013-08-18 00:56:23 -0700

    Really make storage pluggable.

---
+++ transport/filesystem.js
@@ -0,0 +1,11 @@
+var fs = require('fs'),
+  log = process.env['LOG_LIBRARY'] ? require(process.env['LOG_LIBRARY']) : console.log;
+
+module.exports = {
+  save: function(doc, id) {
+    fs.writeFile('./harfiles/' + id + '.har', doc, function(err) {
+      if (err) { return log('EMERGENCY', 'unable to write results file for test ' + id + '. error: ' + err); }
+      log('INFO', 'HAR data written to disk for test job ' + id + '.');
+    });
+  }
+};
